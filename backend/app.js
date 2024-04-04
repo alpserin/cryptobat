17f3187ef5c4a8cc1e9b4d335cd197d24668f37d
Alp Serin <alpsrn@gmail.com>
2024-04-04 19:10:08 +0300

    add frontend

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import axios from 'axios'
+import cors from 'cors'
 import getData from './services/fetchData.js'
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
+app.use(cors())
 
-app.get('/', async (req,res) => {
+app.get('/api', async (req,res) => {
     try {
-        const data = await getData()
-        res.json({data})
+        console.log("GET WORKING")
+        var data = await getData()
+        console.log(data.data);
+        return res.json({data})
     } catch (error) {
         console.error(error)
         res.status(500).json({msg: 'Error fetching data'})
---
+++ frontend/src/components/Table.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Crypto Prices</h2>
+        <div class="data">
+            <div v-for="crypto in crypto_data" :key="index">
+                <p>{{ crypto }}</p>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Table',
+  data() {
+    return {
+      crypto_data: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:5000/api');
+      this.crypto_data = response.data.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
