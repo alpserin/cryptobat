7f0084fc57adf8508808dad9aa06ec0e2b1cbd23
Alp Serin <alpsrn@gmail.com>
2024-04-05 12:19:14 +0300

    round values, add symbols

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -3,9 +3,9 @@
     <h2>Crypto Prices</h2>
     <div class="data">
       <div v-for="crypto in crypto_data" :key="index">
-        <p>Name : {{ crypto.name }}</p>
-        <p>Price ($):{{ crypto.priceUsd }}</p>
-        <p>Price ($):{{ crypto.changePercent24Hr }}</p>
+        <p>Name : {{ crypto.name }} ({{ crypto.symbol }})</p>
+        <p>Rounded Price ($): {{ roundValue(crypto.priceUsd) }}</p>
+        <p>Change in 24hr ($): {{ roundValue(crypto.changePercent24Hr) }} %</p>
         <p>-------------------</p>
       </div>
     </div>
@@ -21,6 +21,9 @@ const crypto_data = ref([]);
 
 onMounted(async () => {
   crypto_data.value = await getCurrentData();
-  console.log(crypto_data.value);
 });
+
+function roundValue(number) {
+  return Math.round(number * 1000000) / 1000000;
+}
 </script>
